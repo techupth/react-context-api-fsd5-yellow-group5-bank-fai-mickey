82fe1b63ea8901ceb59fd12bf7a18c16b5e7bcf3
datfoi <jengjiening@gmail.com>
2024-01-03 13:40:54 +0700

    feat: assignment-context-api/fai

--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,13 @@
+import { UserDataContext } from "../contexts/userDataContext";
+import { useContext } from "react";
+
 function AppHeader() {
+  const contextData = useContext(UserDataContext);
+
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {contextData.username}</h2>
     </div>
   );
 }
